2888a54f7c221266275651ddcdad1aa2e71e2540
DESKTOP-NMFG90O\seokwon <swseong@vvnst.com>
2025-07-09 13:21:36 +0900

    feat: UI 수정

--- src/compoents/HelloVue.vue
+++ src/compoents/HelloVue.vue
@@ -1,14 +1,13 @@
 <template>
-    <div>
-        안녕하세요
-    </div>
+  <div>안녕하세요</div>
+  <div>patch 테스트</div>
 </template>
 <script setup>
-import { onMounted,ref } from "vue";
+import { onMounted, ref } from "vue";
 
 const count = ref(0);
 
 onMounted(() => {
-    console.log(" Hello Vue")
-})
+  console.log(" Hello Vue");
+});
 </script>
